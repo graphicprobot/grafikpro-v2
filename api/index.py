bc564d666d70ae85cb0d484f40742289b061a8c0
graphicprobot <markhaddadin05@icloud.com>
2026-04-24 18:39:31 +0300

    Добавил app = handler для Vercel

--- api/index.py
+++ api/index.py
@@ -775,3 +775,4 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200); self.send_header('Content-type','application/json'); self.end_headers()
         self.wfile.write(json.dumps({"status":"bot online"}).encode()) 
+        app = handler
